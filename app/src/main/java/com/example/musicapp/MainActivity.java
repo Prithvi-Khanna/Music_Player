4133d2db0da6fa3729d3d22b5a0313702db02e21
Prithvi <prithvikhanna10@gmail.com>
2020-02-06 00:54:10 +0530

    updated

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,9 +13,11 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    Button play,pause,stop;
+
+
+    Button play;
     MediaPlayer media;
     int currentPosition;
     SeekBar songSeek;
@@ -28,104 +32,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         play = (Button) findViewById(R.id.play);
-        pause = (Button) findViewById(R.id.pause);
-        stop = (Button) findViewById(R.id.stop);
 
         elapsed = (TextView) findViewById(R.id.elapsed);
         remaining = (TextView) findViewById(R.id.remaining);
 
-
-        media = media.create(this,R.raw.nazron);
+        media = MediaPlayer.create(this,R.raw.nazron);
         media.setLooping(true);
+        media.seekTo(0);
         media.setVolume(0.5f,0.5f);
         totalTime = media.getDuration();
-        media.start();
 
         songSeek = (SeekBar) findViewById(R.id.SeekBar);
-        songSeek.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        if(fromUser)
-                        {
-                            media.seekTo(progress);
-                            songSeek.setProgress(progress);
-                        }
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-
-                    }
+        songSeek.setMax(totalTime);
+        songSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser)
+                {
+                    media.seekTo(progress);
+                    songSeek.setProgress(progress);
                 }
-        );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
         volumeBar = (SeekBar) findViewById(R.id.volumeBar);
-        volumeBar.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        float volumeNum = progress /100f ;
-                        media.setVolume(volumeNum , volumeNum);
-                    }
+         volumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+             @Override
+             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                 float volume = progress / 100f;
+                 media.setVolume(volume,volume);
+             }
 
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
+             @Override
+             public void onStartTrackingTouch(SeekBar seekBar) {
 
-                    }
+             }
 
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
+             @Override
+             public void onStopTrackingTouch(SeekBar seekBar) {
 
-                    }
-                }
-        );
+             }
+         });
 
-        play.setOnClickListener(this);
-        pause.setOnClickListener(this);
-        stop.setOnClickListener(this);
+         new Thread(new Runnable() {
+             @Override
+             public void run() {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while ( media != null )
-                {
-                    try {
-                        Message msg = new Message();
-                        msg.what = media.getCurrentPosition();
-                        handler.sendMessage(msg);
-                        Thread.sleep(1000);
+                 while (media != null)
+                 {
+                     try {
+                     Message m = new Message();
+                     m.what = media.getCurrentPosition();
+                     handler.sendMessage(m);
 
-                    }
-                    catch (InterruptedException e)
-                    {}
-                }
-            }
-        }).start();
+                         Thread.sleep(1000);
+                     } catch (InterruptedException e) {
 
+                     }
 
+                 }
+             }
+         }).start();
     }
 
-    private Handler handler = new Handler()
-    {
+    private Handler handler = new Handler(){
         @Override
-        public void handleMessage(Message msg)
+        public void  handleMessage(Message m)
         {
-            int currPosition = msg.what;
-            songSeek.setProgress(currPosition);
+             int position = m.what;
+             songSeek.setProgress(position);
 
-           String elapsedTime = createTimeLabel(currPosition);
-           elapsed.setText(elapsedTime);
+             String elapsedTime = createTimeLabel(position);
+             elapsed.setText(elapsedTime);
 
-           String remainingTime = createTimeLabel(totalTime - currPosition);
-           remaining.setText("- " + remainingTime);
-
+             String remainingTime = createTimeLabel(totalTime - position);
+             remaining.setText("-"+remainingTime);
         }
     };
 
@@ -137,56 +129,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         timeLabel = min + ":";
         if(sec < 10)
-        {
             timeLabel += "0";
-
-        }
         timeLabel += sec;
 
         return timeLabel;
     }
 
-    @Override
-    public void onClick(View v) {
-
-        switch(v.getId())
+    public  void playBtnClick(View view)
+    {
+        if(!media.isPlaying())
         {
-            case R.id.play:
-                if(media == null)
-                {
-                    media = media.create(getApplicationContext(),R.raw.nazron);
-                    media.setLooping(true);
-                    media.setVolume(0.5f,0.5f);
-                    totalTime = media.getDuration();
-                    media.start();
-                }
+            media.start();
+            play.setBackgroundResource(R.drawable.ic_pause_circle_filled_black_24dp);
 
-                else if(! media.isPlaying())
-                {
-                    media.seekTo(currentPosition);
-                    media.start();
-                }
-            break;
-
-            case R.id.pause:
-                if(media != null)
-                {
-                    media.pause();
-                    currentPosition = media.getCurrentPosition();
-
-                }
-                break;
-
-            case R.id.stop:
-                if(media != null)
-                {
-                    media.stop();
-                    Message msg = new Message();
-                    msg.what = 0;
-                    handler.sendMessage(msg);
-                    media = null;
-                }
-                break;
         }
+        else {
+            media.pause();
+            play.setBackgroundResource(R.drawable.ic_play_circle_filled_black_24dp);
+        }
+
     }
+
+
 }
